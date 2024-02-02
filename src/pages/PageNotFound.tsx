62e3a936240017d95ea004c2599dea8418477c73
d5ng <dong5ffice@gmail.com>
2024-02-02 17:19:11 +0900

    Feat: 404페이지 수정정

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -5,16 +5,19 @@ import { Link } from "react-router-dom";
 function PageNotFound() {
   return (
     <section className={styles.pageNotFound}>
-      <div className={styles.container}>
-        <p className={styles.description}>
-          빈 상자와 같은 이 페이지에서 여러분의 아이디어를 채워보세요.
-          <br />
-          스터디나 프로젝트를 기다리고 있습니다. 🙂
-        </p>
+      <div className={`${styles.container} container`}>
+        <div className={styles.descriptionContent}>
+          <p className={styles.description}>
+            <strong>앗, 여기는 아무 것도 없어요!</strong>
+            <br />
+            정확한 주소를 입력하셨는지 확인하고, 다른 곳을 찾아보세요! 🙂
+          </p>
 
-        <Link to="/posts/write" className={styles.linkTo}>
-          스터디 모집하기
-        </Link>
+          <Link to="/posts/write" className={styles.linkTo}>
+            스터디 보러가기
+          </Link>
+        </div>
+        <div className={styles.imageContent}>404</div>
       </div>
     </section>
   );
