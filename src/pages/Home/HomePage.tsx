db702b9ae285c14c56657e4bbb1e05f8cbf4ee29
78-artilleryman <ywy040150@gmail.com>
2024-02-19 14:44:19 +0900

    style: search bar 배치(기능 미구현)

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,14 @@
 import PostList from "components/Posts/postList/PostList";
 import styles from "./Home.module.scss";
 import FilterList from "components/SelectBox/Filter/FilterList";
+import SearchBar from "components/Common/SearchBar";
 
 function HomePage() {
   return (
     <>
       <section className={styles.filterSection}>
         <FilterList />
-        <div>서치바 영역</div>
+        <SearchBar />
       </section>
       <section className={styles.postSection}>
         <PostList />
